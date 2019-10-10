616738cb3e3c6f73bf5913499667445617f42f95
Corbin Brockbank <corbinbrckbnk76@gmail.com>
2019-10-10 15:20:15 -0600

    added content

--- task-counter/src/components/counter/index.js
+++ task-counter/src/components/counter/index.js
@@ -5,15 +5,25 @@ function Counter() {
 
     const [count, setCount] = useState(0)
 
+    const [name, setName] = useState("")
+
 
 
     return(
         <div>
-            <h2>YOU HAVE DONE {count} TICKETS TODAY</h2>
-            <button onClick={() => setCount(count + 1)}>+ ADD</button>
-            <button onClick={() => setCount(count - 1)}>- SUBTRACT</button>
-            <br></br>
-            <button onClick={() => setCount(0)}>RESET</button>
+            <div className="input">
+                <h2>Welcome  {name} !</h2>
+                <input type="text" placeholder="Enter your name" onChange={() => setName(name)}></input>
+                <button type="submit" onClick>Submit</button>
+            </div>
+
+            <div className="count-buttons">
+                <h2>YOU HAVE DONE {count} TICKETS TODAY</h2>
+                <button id="add-button" onClick={() => setCount(count + 1)}>+ ADD</button>
+                <button onClick={() => setCount(count - 1)}>- SUBTRACT</button>
+                <br></br>
+                <button onClick={() => setCount(0)}>RESET</button> 
+            </div>
         </div>
     )
 }
