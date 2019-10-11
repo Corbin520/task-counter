7a70bdc8c4d808b3a5b4ab644199806d392360a5
Corbin Brockbank <corbinbrckbnk76@gmail.com>
2019-10-11 14:40:35 -0600

    added date

--- task-counter/src/components/counter/index.js
+++ task-counter/src/components/counter/index.js
@@ -3,22 +3,26 @@ import React, { useState } from "react"
 
 function Counter() {
 
+    // count hook
     const [count, setCount] = useState(0)
 
-
-    // function that will get the value from our input
-
-    // event.target.value
+    // name hook
     const [title, setTitle] = useState("")
+
+    // Date
+    var date = new Date().getDate();
+    var month = new Date().getMonth() + 1;
+    var year = new Date().getFullYear();
     
     return(
         <div>
             <div className="input">
                 <h2>Welcome {title}</h2>
-                <input type="text" placeholder="Enter your name" onChange={event => setTitle(event.target.value)} />
+                <input type="text" placeholder="Name" onChange={event => setTitle(event.target.value)} />
             </div>
 
             <div className="count-buttons">
+                <h3>Todays Date is: {month } / {date} / {year}</h3>
                 <h2>YOU HAVE DONE {count} TICKETS TODAY</h2>
                 <button id="add-button" onClick={() => setCount(count + 1)}>+ ADD</button>
                 <button onClick={() => setCount(count - 1)}>- SUBTRACT</button>
